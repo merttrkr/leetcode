8a0e22b9486157f8fbcec7d31c82366a8228ad31
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2024-06-19 17:58:16 +0300

    initial commit of arrays and trees

---
+++ ArraysAndHashing/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ArraysAndHashing;
+
+public class Solution
+{
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        Dictionary<int,int> dict = new Dictionary<int, int>();
+        foreach (int num in nums)
+        {
+            if (dict.ContainsKey(num))
+            {
+                return true;
+            }
+            dict.Add(num, 1);
+        }
+        return false;
+    }
+    public static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        // Count characters in the first string
+        foreach (char c in s)
+        {
+            if (charCount.ContainsKey(c))
+                charCount[c]++;
+            else
+                charCount[c] = 1;
+        }
+
+        // Subtract characters count based on the second string
+        foreach (char c in t)
+        {
+            if (charCount.ContainsKey(c))
+            {
+                charCount[c]--;
+                if (charCount[c] == 0)
+                    charCount.Remove(c);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // If the dictionary is empty, the strings are anagrams
+        return charCount.Count == 0;
+    }
+    public static IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var map = new Dictionary<string, IList<string>>();
+
+        foreach (var word in strs)
+        {
+            var charCount = new int[26]; // Assuming only lowercase alphabets (a-z)
+
+            foreach (var ch in word)
+            {
+                Console.WriteLine("ch in word " + ch );
+                charCount[ch - 'a']++;
+            }
+
+            var key = string.Join(",", charCount);
+
+            if (!map.ContainsKey(key))
+            {
+                map.Add(key, new List<string>());
+            }
+
+            map[key].Add(word);
+        }
+
+        return map.Values.ToList();
+    }
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1,2,3,4,5 };
+        string s = "ana";
+        string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+        //Console.WriteLine(ContainsDuplicate(nums));
+        //Console.WriteLine(IsAnagram(s, s));
+        GroupAnagrams(strs);
+
+
+    }
+
+}
+    
+
---
+++ leetCodes/reverseBinaryTree.cs
@@ -0,0 +1,64 @@
+﻿
+
+// C# code for the above approach
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodes;
+
+public class BinaryTree
+{
+    public TreeNode root;
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static TreeNode InvertTree(TreeNode root)
+    {
+
+        if (root.left != null && root.right != null)
+        {
+            var temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
+        if (root.left != null)
+        {
+            InvertTree(root.left);
+        }
+        if (root.right != null)
+        {
+            InvertTree(root.right);
+        }
+        return root;
+    }
+    public static void Main(string[] args)
+    {
+        // Construct a tree shown in the above figure
+        BinaryTree tree = new BinaryTree();
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+        tree.root.left.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(5);
+        tree.root.left.right.left = new TreeNode(6);
+        tree.root.left.right.right = new TreeNode(7);
+
+        InvertTree(tree.root);
+
+
+    }
+}
+// Driver code
