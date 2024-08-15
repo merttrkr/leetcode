0360f63cbf4e009760122a19e5e33ad47321ef50
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2024-08-15 16:45:43 +0300

    FindMin and search circle solved

--- leetCodes/BinarySearch/Program.cs
+++ leetCodes/BinarySearch/Program.cs
@@ -158,3 +158,74 @@ int SearchInsert(int[] nums, int target)
     }
     return left;
 }
+//Console.WriteLine(FindMin(new int[] { 3, 4, 5, 1, 2 }));
+int FindMin(int[] nums)
+{
+    // check position 0 if the ^1 and 1 is greater than 0
+    // find a number that has a position x
+    // x < x-1 && x < x + 1
+    // 3,4,5,6,7,8,0,1,2
+    int left = 1;
+    int right = nums.Length - 2;
+    if (nums[0] < nums[1] && nums[0] < nums[^1]) return nums[0];
+    if (nums[^1] < nums[0] && nums[^1] < nums[^2]) return nums[^1];
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < nums[mid + 1] && nums[mid] < nums[mid - 1])
+        {
+            return nums[mid];
+        }
+        if (nums[mid] > nums[right])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+
+    }
+    return nums[left];
+}
+Console.WriteLine(SearchCircle(new int[] { 1, 3 },3));
+int SearchCircle(int[] nums, int target)
+{
+    int left = 0;
+    int right = nums.Length - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        //left side is sorted
+        if (nums[mid] >= nums[right] )
+        {
+            if (target < nums[mid] && target >= nums[left])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        else
+        {
+            if (target > nums[mid] && target <= nums[right])
+            {
+                left = mid + 1;
+
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
